564753f82331bebd12298a872a45bd8a8d51952f
kalex1312 <118289733+kalex1312@users.noreply.github.com>
2022-11-26 19:28:31 +0300

    Update task 6.py

--- task 6.py
+++ task 6.py
@@ -3,8 +3,8 @@ list_numbers = [2, 90, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38
 max_number = list_numbers[0]
 max_ind = 0
 
-last_ind = len(list_numbers) - 1
-last_number = list_numbers[last_ind]
+
+last_number = list_numbers[-1]
 
 
 for i, current_number in enumerate(list_numbers):
@@ -13,7 +13,7 @@ for i, current_number in enumerate(list_numbers):
         max_ind = i
 
 
-list_numbers[last_ind] = max_number
-list_numbers[max_ind] = last_number
+list_numbers[-1], list_numbers[max_ind] = max_number, last_number
 
 print(list_numbers)  # Ответ [2, 90, -2, 8, -36, -44, -1, -85, -14, 25, -22, -90, -100, -8, 38, -92, -45, 67, 53, 90]
+
