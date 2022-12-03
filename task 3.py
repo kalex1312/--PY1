80a2c831c1a572ccc99d1cd72e6357e429da9abd
kalex1312 <118289733+kalex1312@users.noreply.github.com>
2022-12-04 00:59:48 +0300

    Лабораторная работа №4

---
+++ task 3.py
@@ -0,0 +1,13 @@
+def delete(list_, index=None):
+      # TODO реализовать функцию удаления элемента из списка по индексу
+
+      if index == None:
+          list_ = list_[:-1]
+      else:
+          list_ = list_[:index] + list_[index + 1:]
+      return list_
+
+
+print(delete([0, 1, 2], index=0))  # [0, 1]
+print(delete([0, 1, 2], index=1))  # [0, 2]
+print(delete([0, 1, 2]))  # [0, 1]
