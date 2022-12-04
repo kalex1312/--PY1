b1391e8a4117c97889af77b45a4904ed1969d567
kalex1312 <118289733+kalex1312@users.noreply.github.com>
2022-12-04 16:36:03 +0300

    Update task 3.py

--- task 3.py
+++ task 3.py
@@ -1,11 +1,18 @@
 def delete(list_, index=None):
       # TODO реализовать функцию удаления элемента из списка по индексу
 
-      if index == None:
-          list_ = list_[:-1]
-      else:
+      if index == None:  # или задать значение по умолчанию вх параметра delete(list_, index=-1)
+          index = -1
+
+      if index < 0:
+          index += len(list_)
+
+      if index <= len(list_) and index >= 0:  # рассматриваем только значения индекса, не "out of range"
+
           list_ = list_[:index] + list_[index + 1:]
-      return list_
+
+      return (list_)
+
 
 
 print(delete([0, 1, 2], index=0))  # [0, 1]
