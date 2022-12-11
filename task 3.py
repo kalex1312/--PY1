9cce5d350ad0f633905752b6424b2f78411bebf8
kalex1312 <118289733+kalex1312@users.noreply.github.com>
2022-12-11 14:05:32 +0300

    Update task 3.py

--- task 3.py
+++ task 3.py
@@ -1,12 +1,12 @@
 
 import random
 
-def get_unique_list_numbers() -> list[int]:
+def get_unique_list_numbers(bottom, top, limit_number) -> list[int]:
     ...  # TODO написать функцию для получения списка уникальных целых чисел
 
     list_numbers = []
-    while (len(list_numbers)) < 15:
-       random_int = random.randint(-10, 10)
+    while (len(list_numbers)) <limit_number:
+       random_int = random.randint(bottom, top)
        if random_int not in list_numbers:
            list_numbers.append(random_int)
 
@@ -15,6 +15,6 @@ def get_unique_list_numbers() -> list[int]:
 
 
 
-list_unique_numbers = get_unique_list_numbers()
+list_unique_numbers = get_unique_list_numbers(-10, 10, 15)
 print("результат:", list_unique_numbers)
 print(len(list_unique_numbers) == len(set(list_unique_numbers)))
