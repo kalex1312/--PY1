1891672ca016d207969fc4869da86861656431bd
kalex1312 <118289733+kalex1312@users.noreply.github.com>
2022-12-11 22:48:42 +0300

    Update task 5.py

--- task 5.py
+++ task 5.py
@@ -3,8 +3,8 @@ import string
 def get_random_password(len_=8) -> str:
     ...  # TODO написать функцию генерации случайных паролей
 
-    letter_list = [str for str in string.ascii_letters+string.digits]
-    pwd = ''.join(random.sample (letter_list, len_))
+    pwd = ''.join(random.sample(string.ascii_letters+string.digits, len_))
+
     return pwd
 
 print(get_random_password(8))
