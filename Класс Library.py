4f0d6a85d2427a82450754a7ae90843503c36102
kalex1312 <118289733+kalex1312@users.noreply.github.com>
2023-01-28 23:15:26 +0300

    Update Класс Library.py

--- Класс Library.py
+++ Класс Library.py
@@ -15,17 +15,42 @@ BOOKS_DATABASE = [
 class Book:
     def __init__(self, id: int, name: str, pages: int):
 
-        self.id =id
+        """
+        Создание и подготовка к работе объекта "Книга"
+        :param id_: Идентификатор книги
+        :param name_: Название книги
+        :param pages_: Количество страниц в книге
+        """
+
+        if not isinstance(id, (int)):
+            raise TypeError("Идентификатор книги должен быть типа int")
+        if id <= 0:
+            raise ValueError("Идентификтаор должен задаваться положительным числом")
+        self.id = id
+
         self.name = name
+
+        if not isinstance(pages, (int)):
+            raise TypeError("Кол-во страниц должны быть типа int")
+        if pages <= 0:
+            raise ValueError("Кол-во страниц должны задаваться положительным числом")
         self.pages = pages
 
 
 class Library:
-    def __init__(self, books=None):
+    def __init__(self, books:list[Book]=[]):
+
+        """
+        Создание и подготовка к работе объекта "Библиотека"
+        :param books_: Список книг
+        """
+
+        if not isinstance(books, list):
+            raise TypeError("Параметр books должен быть задан списком")
         self.books = books
 
     def get_next_book_id(self) -> int:
-        return 1 if self.books == None else len(self.books) + 1
+        return 1 if self.books == [] else len(self.books) + 1
 
     def get_index_by_book_id(self, book_id: int) -> int:
         if not isinstance(book_id, int):
